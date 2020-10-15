f6644b555a351eb8112e3ca501f11d15e316026a
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-10-15 15:10:21 +0800

    add test cases

--- test/range_union_test.rb
+++ test/range_union_test.rb
@@ -15,4 +15,49 @@ class RangeUnionTest < Minitest::Test
       :after  => [5..10]
     )
   end
+
+  def test_when_full_cover_excluded_range
+    assert_before_and_after(
+      proc{ @degree_range.ranges },
+      proc{ @degree_range |= 50..120 },
+      :before => [0...360],
+      :after  => [0...360]
+    )
+  end
+
+  def test_when_right_cover_excluded_range
+    assert_before_and_after(
+      proc{ @degree_range.ranges },
+      proc{ @degree_range |= -10..20 },
+      :before => [0...360],
+      :after  => [-10..359]
+    )
+  end
+
+  def test_when_left_cover_excluded_range
+    assert_before_and_after(
+      proc{ @degree_range.ranges },
+      proc{ @degree_range |= 330..400 },
+      :before => [0...360],
+      :after  => [0..400]
+    )
+  end
+
+  def test_when_not_cover_excluded_range
+    assert_before_and_after(
+      proc{ @degree_range.ranges },
+      proc{ @degree_range |= -30..400 },
+      :before => [0...360],
+      :after  => [-30..400]
+    )
+  end
+
+  def test_multi_range
+    assert_before_and_after(
+      proc{ @multi_range.ranges },
+      proc{ @multi_range |= 50..450 },
+      :before => [0..100, 200..300, 500..600],
+      :after  => [0..450, 500..600]
+    )
+  end
 end
