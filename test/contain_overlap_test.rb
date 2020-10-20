c28f2915f888d156f88006c38c458799ce7ee12f
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-10-20 16:19:14 +0800

    add test cases

---
+++ test/contain_overlap_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ContainOverlapTest < Minitest::Test
+  def setup
+  end
+
+  def test_empty_range
+    assert_equal false, MultiRange.new([]).contain_overlops?
+  end
+
+  def test_when_have_only_one_range
+    assert_equal false, MultiRange.new([-9999, 9999]).contain_overlops?
+  end
+
+  def test_when_range_have_excluded_right_value
+    assert_equal false, MultiRange.new([0...5, 5..10, 20..50]).contain_overlops?
+    assert_equal true, MultiRange.new([0..5, 5..10, 20..50]).contain_overlops?
+  end
+
+  def test_when_full_cover_excluded_range
+    assert_equal true, MultiRange.new([0...360, 50..120]).contain_overlops?
+  end
+
+  def test_when_right_cover_excluded_range
+    assert_equal true, MultiRange.new([0...360, -10..20]).contain_overlops?
+  end
+
+  def test_when_left_cover_excluded_range
+    assert_equal true, MultiRange.new([0...360, 330..400]).contain_overlops?
+  end
+
+  def test_when_not_cover_excluded_range
+    assert_equal true, MultiRange.new([0...360, -30..400]).contain_overlops?
+  end
+
+  def test_float
+    assert_equal false, MultiRange.new([1.2..1.5, 1.7..1.8, 3.5..7.2, 1.6..1.65]).contain_overlops?
+    assert_equal false, MultiRange.new([1.2..1.5, 1.7..1.8, 3.5..7.2, 1.6..1.69]).contain_overlops?
+    assert_equal false, MultiRange.new([1.2..1.5, 1.7..1.8, 3.5..7.2, 1.6...1.7]).contain_overlops?
+    assert_equal true, MultiRange.new([1.2..1.5, 1.7..1.8, 3.5..7.2, 1.6..1.7]).contain_overlops?
+    assert_equal true, MultiRange.new([1.2..1.5, 1.7..1.8, 3.5..7.2, 1.6..1.71]).contain_overlops?
+  end
+end
