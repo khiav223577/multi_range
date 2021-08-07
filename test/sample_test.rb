c6b3e0642e91eaa3736e7655358b41a1900d94d2
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2021-08-07 23:38:59 +0800

    add test cases

---
+++ test/sample_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SampleTest < Minitest::Test
+  def setup
+    srand(123456789)
+  end
+
+  def test_empty_range
+    assert_equal nil, MultiRange.new([]).sample
+  end
+
+  def test_one_element_range
+    assert_equal 28, MultiRange.new([28..28]).sample
+  end
+
+  def test_ranges
+    assert_equal 92, MultiRange.new([28..28, 90..100]).sample
+  end
+end
